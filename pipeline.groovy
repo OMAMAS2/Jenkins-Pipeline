f9789618bef22f711ebc6e382580416fdbe583dc
OMAMAS2 <142631007+OMAMAS2@users.noreply.github.com>
2024-09-03 00:17:35 +1000

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,16 +1,54 @@
 pipeline {
     agent any
-    stages{
+    
+    environment {
+        DIRECTORY_PATH = 'C:\\Users\\omama\\Jenkins'
+        TESTING_ENVIRONMENT = 'PipilineTest1'
+        PRODUCTION_ENVIRONMENT = 'OmamaS'
+    }
+
+    stages {
         stage('Build') {
             steps {
-                echo "Building..."
+                echo "Fetch the source code from the directory path specified by the environment variable: ${env.DIRECTORY_PATH}"
+                echo "Compile the code and generate any necessary artifacts"
             }
             post{
                 success{
                     mail to: "omamashakhli2@gmail.com",
                     subject: "Build Status Email",
                     body: "Build was Successful!"
-                }
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                echo "Unit tests"
+                echo "Integration tests"
+            }
+        }
+        
+        stage('Code Quality Check') {
+            steps {
+                echo "Check the quality of the code"
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                echo "Deploy the application to a testing environment specified by the environment variable: ${env.TESTING_ENVIRONMENT}"
+            }
+        }
+        
+        stage('Approval') {
+            steps {
+                sleep(time: 10, unit: 'SECONDS')
+            }
+        }
+        
+        stage('Deploy to Production') {
+            steps {
+                echo "Deploy the code to the production environment specified by the environment variable: ${env.PRODUCTION_ENVIRONMENT}"
             }
         }
     }
